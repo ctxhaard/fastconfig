b13f2b066dba90eea3902371e7a656a49a104a44
Carlo Tomasin <ctomasin@mb-swmobiletemp.italia.texa.org>
2018-04-24 11:34:15 +0200

    bugfix

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-DIR=$(dirname "${BASH_SOURCE[0]}")
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 mkdir -p ~/.ssh/
+mkdir -p ~/.vim/bundle/
 
 ln -s $DIR/ssh-config ~/.ssh/config
 ln -s $DIR/texarc     ~/.texarc
-ln -s $DIR/vimrc      ~/.vimr.c
+ln -s $DIR/vimrc      ~/.vimrc
+
+echo source ~/.texarc >> ~/.bashrc
+echo source ~/.texarc >> ~/.bash_profile
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall
 
