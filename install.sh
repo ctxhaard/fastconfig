6df4ad2726c0e5fe2b30a47343842bb70c7b715e
Carlo Tomasin <carlo.tomasin@texa.com>
2018-05-23 08:51:30 +0200

    improved

--- install.sh
+++ install.sh
@@ -6,9 +6,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p ~/.ssh/
 mkdir -p ~/.vim/bundle/
 
+rm ~/.ssh/config
 ln -s $DIR/ssh-config ~/.ssh/config
-ln -s $DIR/texarc     ~/.texarc
-ln -s $DIR/vimrc      ~/.vimrc
+
+rm ~/.texarc
+ln -s $DIR/texarc ~/.texarc
+
+rm ~/.vimrc
+ln -s $DIR/vimrc ~/.vimrc
+
+rm ~/mount_ctomasin.sh
+ln -s $DIR/mount_ctomasin.sh ~/mount_ctomasin.sh
 
 echo source ~/.texarc >> ~/.bashrc
 echo source ~/.texarc >> ~/.bash_profile
--- mount_ctomasin.sh
+++ mount_ctomasin.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+echo decrypting secure partition...
+echo Type the partition decrypt password NOT THE DOMAIN PASSWORD!
 udisksctl unlock -b /dev/sda2
 udisksctl mount -b /dev/dm-0
 
 ORIGIN_BASE=/media/$(id -un)/texa-enc
+echo mounting secure folders...
 sudo mount --bind ${ORIGIN_BASE}/development ~/development
 sudo mount --bind ${ORIGIN_BASE}/DocumentsReserved ~/DocumentsReserved
 sudo mount --bind ${ORIGIN_BASE}/opt /opt
+echo mounting remote folders...
 sudo mount -t cifs //texa-nas.italia.texa.org/homeusers/ctomasin ~/Documents -o username=ctomasin,noexec,uid=$(id -u),gid=$(id -g),file_mode=0700
 sudo mount -t cifs //texa-nas.italia.texa.org/pub/temp ~/Temp -o username=ctomasin,noexec,uid=$(id -u),gid=$(id -g)
-
+echo done.
