898bc2a9454f4751c18f9daf630481d71370cf8f
Carlo Tomasin <carlo.tomasin@texa.com>
2018-09-25 09:12:24 +0200

    some changes

--- mount_ctomasin.sh
+++ mount_ctomasin.sh
@@ -13,13 +13,15 @@
 #sudo mount --bind ${ORIGIN_BASE}/DocumentsReserved ~/DocumentsReserved
 #sudo mount --bind ${ORIGIN_BASE}/opt /opt
 echo mounting remote folders...
+echo mounting remote documents...
 mkdir -p ~/Documents
 sudo mount -t cifs //texa-nas.italia.texa.org/homeusers/ctomasin ~/Documents -o username=ctomasin,noexec,uid=$(id -u),gid=$(id -g),file_mode=0700
+echo mounting remote temp...
 mkdir -p ~/Temp
 sudo mount -t cifs //texa-nas.italia.texa.org/pub/temp ~/Temp -o username=ctomasin,noexec,uid=$(id -u),gid=$(id -g)
 
 echo mounting folder on external disk to use with Virtual Manager...
 mkdir -p /opt/virt-manager/
-sudo mount --bind /media/$(id -u)/ctomasin-ext/virt-manager /opt/virt-manager
+sudo mount --bind /media/$(id -nu)/ctomasin-ext/virt-manager /opt/virt-manager
 echo done.
 
